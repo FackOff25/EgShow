e054fd76488d8afc374540ab28a8ad4be936a005
FackOff25 <kozvladimir@bk.ru>
2022-06-20 18:28:00 +0300

    First dzi load

--- static/js/image-show.js
+++ static/js/image-show.js
@@ -1,27 +1,11 @@
 var viewer;
 
-function openOpenSeaDragon(fileInput) {
-    const loader = document.getElementById('loader_box');
-    loader.style.display = 'none';
-
-    const shower = document.getElementById('openSeaDragon-window');
-    shower.style.display = 'block';
-
-    const file = fileInput.files[0];
-    const image = URL.createObjectURL(file);
+function onLoad() {
     viewer = OpenSeadragon({
         id: "openSeaDragon-window",
         prefixUrl: "../static/openseadragon/images/",
-        tileSources: {
-            type: 'image',
-            url: image,
-        },
+        tileSources: "../static/img/example.dzi"
     });
-
 }
 
-function onLoad() {
-            //TODO:add openOpenSeaDragon(image of the page)
-        }
-
-        window.onload = onLoad();
+window.onload = onLoad();
